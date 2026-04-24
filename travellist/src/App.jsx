3af5954f23891de04642366da10dda97a17ccedd
Prajjwal Budha <prajjwalbudha@gmail.com>
2026-04-24 07:49:53 +0545

    Added needed component

---
+++ travellist/src/App.jsx
@@ -0,0 +1,30 @@
+
+function App() {
+  return (
+    <div className="App">
+    <Logo />
+    <Form />
+    <PackingList/>
+    <Stats />
+    </div>
+  );
+}
+
+function Logo(){
+  
+
+}
+function Form(){
+  
+
+}
+function PackingList(){
+  
+
+}
+function Stats(){
+  
+
+}
+
+export default App;
