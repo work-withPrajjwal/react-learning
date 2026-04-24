e6316b2fa2bccac4a334f27c9bd47c834a1bf7b6
Prajjwal Budha <prajjwalbudha@gmail.com>
2026-04-24 07:30:40 +0545

    Updated counter-date

--- counter-date/src/App.jsx
+++ counter-date/src/App.jsx
@@ -2,21 +2,36 @@
 import {useState} from 'react'
 
 function App() {
-  const 
+  const[count , setCount] = useState(0)
+  const[step , setStep] = useState(1);
+  const date = new Date();
+  date.setDate(date.getDate()+ count);
   return (
-    <div>
-      <div className='step'>
-
-      <button>+</button>
-      <button>-</button>
+    <div className='container'>
+      <div className="step">
+        <button onClick={() => setStep((currStep) => step + 1)}>+</button>
+        <span>Step:{step}</span>
+        <button onClick={() => setStep((currStep) => step - 1)}>-</button>
       </div>
-      <div className='count'>
-      <button>+</button>
-      <span>Count:{}</span>
-      <button>-</button>
+      <div className="count">
+        <button onClick={() => setCount((currCount) => currCount + step)}>
+          +
+        </button>
+        <span>Count:{count}</span>
+        <button onClick={() => setCount((currCount) => currCount - step)}>
+          -
+        </button>
       </div>
+      <p>
+        {
+          count ===0 ? "Today is" : count > 0 ? `${count} day from today` : `${-count} day ago was`
+        }
+        <span>
 
-
+        {date.toDateString()}
+        </span>
+        </p>
+        
     </div>
   );
 }
