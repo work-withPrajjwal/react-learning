0cc35d4bdc1d25a4fdaaea112b764d8eba10a8e7
Prajjwal Budha <prajjwalbudha@gmail.com>
2026-04-29 07:59:24 +0545

    Added more features inside form

--- travellist/src/App.jsx
+++ travellist/src/App.jsx
@@ -1,44 +1,78 @@
+import { useState } from "react";
+
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
 
 function App() {
   return (
     <div className="App">
-    <Logo />
-    <Form />
-    <PackingList/>
-    <Stats />
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
     </div>
   );
 }
 
-function Logo(){
+function Logo() {
+  return <h1>🏝️ FAR AWAY 🎒</h1>;
+}
+function Form() {
+  const[description, setDescription] = useState("");
+  const[quantity, setQuantity] = useState(1);
+  function handleAdd(e){
+    e.preventDefault();
+    
+  }
+  return (
+    <form className="add-form">
+      <h3>What do you need for your Trip 😍 ?</h3>
+      <select value={quantity} onChange={(e)=> setQuantity(e.target.value)}>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value="{num}">{num}</option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="item...."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
+  );
+}
+function PackingList() {
+  function handleDeleteItem(e) {
+    e.span.value.remove();
+  }
+  return (
+    <ul className="list">
+    {initialItems.map((item)=>(
+      <Item key ={item.id} item={item} />
+    ))}
+    </ul>
+  );
+}
+function Item({item}){
   return(
-    <h1>🏝️ FAR AWAY 🎒</h1>
+    <li className="item">
+      <input type ="checkbox" />
+      <span>
+        {item.description} {item.quantity}
+      </span>
+      <button>❌</button>
+    </li>
   )
-
 }
-function Form(){
-  return(
-    <div className="add-form">
-      <h3>What do you need for your 😍 Trip?</h3>
-      <input type="text" />
-    </div>
-  )
-
-}
-function PackingList(){
-  return <div className="list">
-    <span>Shirt</span>
-    <button>❌</button>
-  </div>
-
-}
-function Stats(){
-  return(
+function Stats() {
+  return (
     <footer className="stats">
-      🧳 You have  1 item  on your list and you already packed x.
+      🧳 You have 1 item on your list and you already packed x.
     </footer>
-  )
-
+  );
 }
 
 export default App;
