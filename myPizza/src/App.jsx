92dfab2b66e549f1123af9bfd64d3fcce10e420b
Prajjwal Budha <prajjwalbudha@gmail.com>
2026-04-30 08:03:39 +0545

    Kept only neccessary files for the project

--- myPizza/src/App.jsx
+++ myPizza/src/App.jsx
@@ -1,11 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import "./index.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -116,7 +112,7 @@ function App() {
       <div className="ticks"></div>
       <section id="spacer"></section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
