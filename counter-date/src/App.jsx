83d37ae54f0441ffa85bb0d4288c0ebdd8ce3305
Prajjwal Budha <prajjwalbudha@gmail.com>
2026-04-30 20:00:07 +0545

    Succesfully added input

--- counter-date/src/App.jsx
+++ counter-date/src/App.jsx
@@ -6,32 +6,69 @@ function App() {
   const[step , setStep] = useState(1);
   const date = new Date();
   date.setDate(date.getDate()+ count);
+
+  function inputStep(e){
+    setStep(parseInt(e.target.value))
+  }
+   function inputCount(e) {
+     setCount(parseInt(e.target.value));
+   }
+   function reset(){
+    setCount(0)
+   }
+   function range(e){
+    setCount(parseInt(e.target.value))
+   }
   return (
-    <div className='container'>
+    <div className="container">
+      <label for="points">Points (between 0 and 10):</label>
+      <input
+        className="range"
+        onChange={range}
+        type="range"
+        id="points"
+        name="points"
+        min="0"
+        max="10"
+      />
+
       <div className="step">
-        <button onClick={() => setStep((currStep) => step + 1)}>+</button>
-        <span>Step:{step}</span>
-        <button onClick={() => setStep((currStep) => step - 1)}>-</button>
-      </div>
-      <div className="count">
-        <button onClick={() => setCount((currCount) => currCount + step)}>
+        <button className="add" onClick={() => setStep((currStep) => step + 1)}>
           +
         </button>
+        <input type="number" onChange={inputStep} />
+        <span>Step:{step}</span>
+        <button className="sub" onClick={() => setStep((currStep) => step - 1)}>
+          -
+        </button>
+      </div>
+      <div className="count">
+        <button
+          className="add"
+          onClick={() => setCount((currCount) => currCount + step)}
+        >
+          +
+        </button>
+        <input type="number" onChange={inputCount} />
         <span>Count:{count}</span>
-        <button onClick={() => setCount((currCount) => currCount - step)}>
+        <button
+          className="sub"
+          onClick={() => setCount((currCount) => currCount - step)}
+        >
           -
         </button>
       </div>
       <p>
-        {
-          count ===0 ? "Today is" : count > 0 ? `${count} day from today` : `${-count} day ago was`
-        }
-        <span>
-
-        {date.toDateString()}
-        </span>
-        </p>
-        
+        {count === 0
+          ? "Today is"
+          : count > 0
+            ? `${count} day from today is `
+            : `${-count} day ago was`}
+        <span>{date.toDateString()}</span>
+      </p>
+      <button className="reset" onClick={reset}>
+        Reset
+      </button>
     </div>
   );
 }
