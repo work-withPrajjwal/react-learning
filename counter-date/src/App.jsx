dd02fd17fa75dcf5fac8a0296ea50b2a016a17d4
Prajjwal Budha <prajjwalbudha@gmail.com>
2026-05-01 22:03:25 +0545

    Added new feature handleRange

--- counter-date/src/App.jsx
+++ counter-date/src/App.jsx
@@ -3,45 +3,35 @@ import {useState} from 'react'
 
 function App() {
   const[count , setCount] = useState(0)
-  const[step , setStep] = useState(1);
+  const[step , setStep] = useState(0);
   const date = new Date();
   date.setDate(date.getDate()+ count);
 
-  function inputStep(e){
-    setStep(parseInt(e.target.value))
-  }
    function inputCount(e) {
      setCount(parseInt(e.target.value));
    }
    function reset(){
+    setStep(0)
     setCount(0)
    }
-   function range(e){
-    setCount(parseInt(e.target.value))
+   function handleRange(e){
+    setStep(parseInt(e.target.value))
    }
   return (
     <div className="container">
-      <label for="points">Points (between 0 and 10):</label>
+      <label for="points">{step}</label>
       <input
         className="range"
-        onChange={range}
+        onChange={handleRange}
         type="range"
         id="points"
         name="points"
         min="0"
         max="10"
+        value ={step}
       />
 
-      <div className="step">
-        <button className="add" onClick={() => setStep((currStep) => step + 1)}>
-          +
-        </button>
-        <input type="number" onChange={inputStep} />
-        <span>Step:{step}</span>
-        <button className="sub" onClick={() => setStep((currStep) => step - 1)}>
-          -
-        </button>
-      </div>
+  
       <div className="count">
         <button
           className="add"
@@ -49,7 +39,9 @@ function App() {
         >
           +
         </button>
-        <input type="number" onChange={inputCount} />
+        <input 
+        value={count}
+        type="number" onChange={inputCount} />
         <span>Count:{count}</span>
         <button
           className="sub"
