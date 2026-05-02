776dcf9d73770e723b8199b1e6313f1ad6c9ea81
Prajjwal Budha <prajjwalbudha@gmail.com>
2026-05-02 21:29:29 +0545

    Sorting Items completed

--- travellist/src/App.jsx
+++ travellist/src/App.jsx
@@ -68,10 +68,16 @@ function Form({onAddItems}) {
   );
 }
 function PackingList({ items, onDeleteItems, onToggleItems }) {
+  const[sortBy, setSortBy] = useState("input");
+  let sortedItems;
+  if(sortBy==="input") sortedItems =items;
+  if(sortBy==="description") sortedItems= items.slice().sort((a,b)=>a.description.localeCompare(b.description))
+    if(sortBy==='packed') sortedItems = items.slice().sort((a,b)=> Number(a.packed)- Number(b.packed));
+
   return (
     <div className="list">
       <ul className="list">
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             key={item.id}
             item={item}
@@ -80,6 +86,13 @@ function PackingList({ items, onDeleteItems, onToggleItems }) {
           />
         ))}
       </ul>
+      <div className="action">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value='input'>sort By Input Order</option>
+          <option value="description">sort By Description</option>
+          <option value="packed">sort By Packed Status</option>
+        </select>
+      </div>
     </div>
   );
 }
