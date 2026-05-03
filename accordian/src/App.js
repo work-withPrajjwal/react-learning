c3b34e2fc157d898c693ca895ad5c981e934bc17
Prajjwal Budha <prajjwalbudha@gmail.com>
2026-05-03 13:18:04 +0545

    Added Toggle feature and conditional rendering

--- accordian/src/App.js
+++ accordian/src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import { useState } from "react";
 
 const faqs = [
   {
@@ -31,14 +32,21 @@ function Accordion({data}) {
 }
 
 function AccordionItem({title, text, num}){
+const[isOpen, setIsOpen] = useState(false);
+function handleToggle(){
+    setIsOpen((currIsOpen)=> !currIsOpen)
+}
     return(
-        <div className="item">
+        <div className={`item ${isOpen?"open":""}`} onClick={handleToggle}>
             <p className="number">{num <10 ? `0${num+1}`:num+1}</p>
             <p className="title">{title}</p>
-            <p className="icon">-</p>
-            <div className="content-box">
+            <p className="icon">{isOpen ? "-": "+"
+}</p>
+           {isOpen &&
+          <div className="content-box">
                 {text}
             </div>
+            }
 
         </div>
     );
