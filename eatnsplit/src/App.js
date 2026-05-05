586b1553964dac0d34ea6d7bf812d51dfc32821c
Prajjwal Budha <prajjwalbudha@gmail.com>
2026-05-05 23:45:01 +0545

    adding friend state

--- eatnsplit/src/App.js
+++ eatnsplit/src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 const initialFriends = [
   {
     id: 118836,
@@ -20,54 +21,98 @@ const initialFriends = [
 ];
 
 function App() {
-  return <div className="app">
-  <div className="sidebar">
-    <FriendList/>
-    <FormAddFriend/>
-  </div>
-  </div>;
+  const [showAddFriend, setShowAddFriend] = useState(false);
+  function handleShowAddFriend() {
+    setShowAddFriend((currShow) => !currShow);
+  }
+
+  return (
+    <div className="app">
+      <div className="sidebar">
+        <FriendList />
+        {showAddFriend && <FormAddFriend />}
+        <Button onClick={handleShowAddFriend}>
+          {showAddFriend ? "close" : "AddFriend"}
+        </Button>
+      </div>
+      <FormSplitBill />
+    </div>
+  );
 }
 
-
-function FriendList(){
+function FriendList() {
   const friends = initialFriends;
-  return <ul>
-    {
-      friends.map((friend)=>(
-        <Friend friend={friend} key={friend.id}/>
-      ))
-    }
-  </ul>
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend friend={friend} key={friend.id} />
+      ))}
+    </ul>
+  );
 }
 
-function Friend({friend}){
-  return <li>
-  <img src={friend.image} alt={friend.name}/>
-  <h3>{friend.name}</h3>
-  {friend.balance < 0 &&( <p className="red">You owe{friend.name} ${Math.abs(friend.balance)}</p>
-  )}
+function Friend({ friend }) {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+      {friend.balance < 0 && (
+        <p className="red">
+          You owe{friend.name} ${Math.abs(friend.balance)}
+        </p>
+      )}
 
-  {friend.balance > 0 &&( <p className="green">{friend.name} owes you {friend.name} ${Math.abs(friend.balance)}</p>)}
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owes you {friend.name} ${Math.abs(friend.balance)}
+        </p>
+      )}
 
-
-  {friend.balance === 0 &&( <p className="black">You and {friend.name} are even</p>)}
-  <Button>Select</Button>
-  </li>
+      {friend.balance === 0 && (
+        <p className="black">You and {friend.name} are even</p>
+      )}
+      <Button>Select</Button>
+    </li>
+  );
 }
 
-function FormAddFriend(){
- return (
-   <form className="form">
-     <label>👩‍❤️‍👨 Friend Name</label>
-     <input type="text"/>
-     <label>🌆 Image URL</label>
-     <input type="text"/>
-    <Button>Add</Button>
-   </form>
- );
+function FormAddFriend() {
+  return (
+    <form className="form-add-friend">
+      <label>👩‍❤️‍👨 Friend Name</label>
+      <input type="text" />
+      <label>🌆 Image URL</label>
+      <input type="text" />
+      <Button>Add</Button>
+    </form>
+  );
 }
 
-function Button({children}){
-  return <button className="button">{children}</button>
+function Button({ children, onClick }) {
+  return (
+    <button className="button" onClick={onClick}>
+      {children}
+    </button>
+  );
+}
+
+function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>SPLIT A BILL WITH X</h2>
+      <label>💰Bill Value</label>
+      <input type="text" />
+      <label>🧔Your Expense</label>
+      <input type="text" />
+      <label>🙍‍♀️X's Expense</label>
+      <input type="text" disabled />
+      <label>🤑Who is paying the bill</label>
+      <select>
+        <option value="user">You</option>
+        <option value="friend">X</option>
+      </select>
+      <Button>Split bill</Button>
+    </form>
+  );
 }
 export default App;
