deb7ab729ba026a71adf7db29fa2fdd7d5df14a5
Prajjwal Budha <prajjwalbudha@gmail.com>
2026-05-05 15:38:41 +0545

    Added the core logic

--- nextmessage/src/App.js
+++ nextmessage/src/App.js
@@ -21,10 +21,20 @@ function App() {
     if(message>1) setMessage((currMessage)=> currMessage-1)
   }
 
-  function 
+
+  function handleNextMessage(){
+    if(message<data.length) setMessage((currMessage)=>currMessage+1)
+  }
   return (
     <div className="App">
-     
+      <div className="text">
+        <p>{data[message - 1].message}</p>
+        <p>-{data[message -1].name}</p>
+      </div>
+      <div className="buttons">
+      <button onClick={handlePreviousMessage}>Prev</button>
+      <button onClick={handleNextMessage}>Next</button>
+      </div>
     </div>
   );
 }
