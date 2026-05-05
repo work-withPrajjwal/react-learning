51ef8cb5949a0653b9b686d153fb0c41e440459d
Prajjwal Budha <prajjwalbudha@gmail.com>
2026-05-05 15:22:12 +0545

    added previousMessage function

--- nextmessage/src/App.js
+++ nextmessage/src/App.js
@@ -1,9 +1,30 @@
 import { useState } from "react";
 
+
+const data=[
+  {
+    message: "This is the best product I've ever used!",
+    name:"Jane Doe",
+
+  },
+  {
+    message: "I highly recommend this product to everyone!",
+    name:"Jane Doe",
+
+  },
+]
+
 function App() {
+  const[message, setMessage]= useState(1);
+  
+  function handlePreviousMessage(){
+    if(message>1) setMessage((currMessage)=> currMessage-1)
+  }
+
+  function 
   return (
     <div className="App">
-
+     
     </div>
   );
 }
