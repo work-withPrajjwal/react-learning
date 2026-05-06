ca3be638b3962cefaa8ee1c65f7cbc5752c3347e
Prajjwal Budha <prajjwalbudha@gmail.com>
2026-05-06 14:29:29 +0545

    added handleForm

--- eatnsplit/src/App.js
+++ eatnsplit/src/App.js
@@ -23,7 +23,8 @@ const initialFriends = [
 function App() {
   const [showAddFriend, setShowAddFriend] = useState(false);
   const[friends, setFriends] = useState(initialFriends)
-  const[selectedFriend, setSelectedFriend]= useState(null)
+  const[selectedFriend, setSelectedFriend]= useState(null);
+  const[]
   function handleShowAddFriend() {
     setShowAddFriend((currShow) => !currShow);
     
@@ -130,19 +131,34 @@ function FormSplitBill({selectedFriend}) {
 
   function handleSubmit(e){
     e.preventDefault();
+    if(!bill || !paidByUser) return;
+    if(whoIsPaying === "user"){
+      
   }
 
   return (
     <form className="form-split-bill" onSubmit={handleSubmit}>
       <h2>SPLIT A BILL WITH {selectedFriend.name}</h2>
       <label>💰Bill Value</label>
-      <input type="text" value={bill} onChange={(e)=> setBill(Number(e.target.value))} />
+      <input
+        type="text"
+        value={bill}
+        onChange={(e) => setBill(Number(e.target.value))}
+      />
       <label>🧔Your Expense</label>
-      <input type="text"  value={paidByUser} onChange={(e)=>setPaidByUser(Number(e.target.value))}/>
+      <input
+        type="text"
+        value={paidByUser}
+        onChange={(e) => setPaidByUser(
+          Number(e.target.value)> bill? paidByUser: Number(e.target.value))}
+      />
       <label>🙍‍♀️{selectedFriend.name}'s Expense</label>
       <input type="text" disabled value={paidByFriend} />
       <label>🤑Who is paying the bill</label>
-      <select value={whoIsPaying} onChange={(e)=>setWhoIsPaying(e.target.value)}>
+      <select
+        value={whoIsPaying}
+        onChange={(e) => setWhoIsPaying(e.target.value)}
+      >
         <option value="user">You</option>
         <option value="friend">X</option>
       </select>
