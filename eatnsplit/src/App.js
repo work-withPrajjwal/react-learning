00587ac14bc1ad37ac61718182bca8eb4debc536
Prajjwal Budha <prajjwalbudha@gmail.com>
2026-05-06 13:58:35 +0545

    Added bill

--- eatnsplit/src/App.js
+++ eatnsplit/src/App.js
@@ -124,18 +124,25 @@ function Button({ children, onClick }) {
 
 function FormSplitBill({selectedFriend}) {
   const[bill, setBill] = useState("")
-  const[paidByUser, setPaidByUser] = useState("") 
+  const[paidByUser, setPaidByUser] = useState("");
+  const[whoIsPaying, setWhoIsPaying]= useState('')
+  const paidByFriend = bill ? bill - paidByUser: '' ;
+
+  function handleSubmit(e){
+    e.preventDefault();
+  }
+
   return (
-    <form className="form-split-bill">
+    <form className="form-split-bill" onSubmit={handleSubmit}>
       <h2>SPLIT A BILL WITH {selectedFriend.name}</h2>
       <label>💰Bill Value</label>
       <input type="text" value={bill} onChange={(e)=> setBill(Number(e.target.value))} />
       <label>🧔Your Expense</label>
       <input type="text"  value={paidByUser} onChange={(e)=>setPaidByUser(Number(e.target.value))}/>
       <label>🙍‍♀️{selectedFriend.name}'s Expense</label>
-      <input type="text" disabled />
+      <input type="text" disabled value={paidByFriend} />
       <label>🤑Who is paying the bill</label>
-      <select>
+      <select value={whoIsPaying} onChange={(e)=>setWhoIsPaying(e.target.value)}>
         <option value="user">You</option>
         <option value="friend">X</option>
       </select>
