de12115ea2c639c2217059e62fc78e3ed55df31c
Prajjwal Budha <prajjwalbudha@gmail.com>
2026-05-13 21:48:29 +0545

    Added styles in the component

--- popcorn/src/StarRating.jsx
+++ popcorn/src/StarRating.jsx
@@ -1,12 +1,22 @@
-function StarRating(){
-    return(
-        <div>
-           <div>
-            
-           </div>
+function StarRating({maxRating=5}) {
+    const containerStyle={
+        display:"flex",
+        alignItem:"center",
+        gap:"16px"
+    }
+  return (
+    <div style={containerStyle}>
+      <div>
+        {
+            Array.from({length:maxRating}, (_,i)=><span>
+                S{i+1}
+            </span>)
+        }
+      </div>
 
-        </div>
-    )
+      <p>10</p>
+    </div>
+  );
 }
 
 export default StarRating;
--- popcorn/src/index.js
+++ popcorn/src/index.js
@@ -8,7 +8,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating/>
+    <StarRating />
+    <StarRating maxRating={10}/>
   </React.StrictMode>
 );
 
