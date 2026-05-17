de287607140f432dacf35d63209ee98e734ef1eb
Prajjwal Budha <prajjwalbudha@gmail.com>
2026-05-17 19:11:22 +0545

    added displayText using split, slice and join

--- text-expander/src/App.js
+++ text-expander/src/App.js
@@ -38,18 +38,24 @@ export default function App() {
 function TextExpander({
   children,
   collapsedNumWords=10,
-  expandButtonText = 'SHow More',
-  collapseButtonText='SHow Less',
+  expandButtonText = 'Show More',
+  collapseButtonText='Show Less',
   buttonColor='blue', expanded='false', className=''
 }) {
+  
   const[isExpanded, setIsExpanded]= useState(expanded);
+  const displayText = isExpanded
+    ? children
+    : children.split(" ").slice(0, collapsedNumWords).join(" ") + "...";
+
+    const button
   function handleClick(){
     setIsExpanded((isExpanded)=> !isExpanded);
   }
   return(
 <div className={className}>
-  <span>{children}</span>
-  <button onClick={handleClick}>{expandButtonText}</button>
+  <span>{displayText}</span>
+  <button onClick={handleClick}>{isExpanded ? collapseButtonText: expandButtonText }</button>
 </div>
   )
 }
