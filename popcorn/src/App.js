502be9d3d468971ccb1bdc8c9d25ee54395d0e5d
Prajjwal Budha <prajjwalbudha@gmail.com>
2026-05-20 22:01:04 +0545

    Added try and catch block

--- popcorn/src/App.js
+++ popcorn/src/App.js
@@ -12,14 +12,20 @@ const[isLoading, setIsLoading] = useState(false);
 
 useEffect(function (){
 async function fetchMovies(){
-  setIsLoading(true);
+try{
+    setIsLoading(true);
  const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s="inception"`);
+ if(!res.ok)
+ throw new Error("Something went wrong with movie fetching");
  const data = await res.json();
  setMovies(data.Search);
-   
+ setIsLoading(false); 
+}
+catch(err){
+  console.log(err)
+}
 }
 fetchMovies();
-setIsLoading(false);
 },[])
 
 
