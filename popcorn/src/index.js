c3cfa44dd1171724278802d5cde4491539ef22e3
Prajjwal Budha <prajjwalbudha@gmail.com>
2026-05-20 21:17:43 +0545

    fetching data from api

--- popcorn/src/index.js
+++ popcorn/src/index.js
@@ -1,32 +1,15 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './App';
-import StarRating from './StarRating';
+import "./index.css";
+import App from './App';
 
 
-function Test(){
-  const [movieRating, setMovieRating] = useState(0);
 
-  return (
-    <div>
-      <StarRating
-        color="red"
-        maxRating={5}
-        messages={["Bad", "Poor", "Okay", "Good", "Excellent"]} onSetMovieRating={setMovieRating} defaultRating={3}
-      />
-      <p>THis movie was {movieRating} Stars.</p>
-    </div>
-  );
-}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating
-      maxRating='5' messages='hello'
-    />
+    <App />
   </React.StrictMode>,
 );
 
