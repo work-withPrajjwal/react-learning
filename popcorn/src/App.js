71dc4599bce59ec83980554482e59168bd39b162
Prajjwal Budha <prajjwalbudha@gmail.com>
2026-05-24 11:59:31 +0545

    Added abort controller

--- popcorn/src/App.js
+++ popcorn/src/App.js
@@ -36,31 +36,39 @@ function handleDeleteWatched(id){
 
 
 useEffect(function (){
+    const controller = new AbortController();
 async function fetchMovies(){
+
 try {
   setIsLoading(true);
   setError("")
   const res = await fetch(
-    `http://www.omdbapi.com/?apikey=${KEY}&s=${tempQuery}`,
+    `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`, 
+    {signal:controller.signal}
   );
   if (!res.ok) throw new Error("Something went wrong with movie fetching");
   const data = await res.json();
   if (data.Response === "False") throw new Error("Movie Not Found");
-  setError("");
   setMovies(data.Search);
+  setError("");
 } catch (err) {
+  if(err.name !== "AbortError")
   setError(err.message);
 } finally {
   setIsLoading(false);
 }
 }
-if(tempQuery.length < 3){
+if(query.length < 3){
   setMovies([]);
   setError('');
   return;
 }
 fetchMovies();
-},[])
+
+return function(){
+  controller.abort()
+}
+},[query])
 
 
 
