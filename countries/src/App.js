bd4918043a4f47d2de764c0fa3bcefcac2a2f0fc
Prajjwal Budha <prajjwalbudha@gmail.com>
2026-05-25 22:03:00 +0545

    {

--- countries/src/App.js
+++ countries/src/App.js
@@ -1,9 +1,23 @@
+export default function App(){
+return (
+  <div className="container">
 
-function App() {
-  return (
-    <div>
-    </div>
-  );
+  <Header/>
+  </div>
+)
 }
 
-export default App;
+
+function Header(){
+return(
+  <div className="header">
+    <h4>Where in the world?</h4>
+    <button>
+      <span>
+      🌑
+      </span>
+      dark Mode</button>
+
+  </div>
+)
+}
