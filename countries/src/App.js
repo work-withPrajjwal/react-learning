f9c68bb333dbf0691e4de293e19a32054ceef9ba
Prajjwal Budha <prajjwalbudha@gmail.com>
2026-05-25 22:40:24 +0545

    {

--- countries/src/App.js
+++ countries/src/App.js
@@ -1,16 +1,45 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export default function App(){
-  const [region, setRegion] = useState("")
+  const [region, setRegion] = useState("asia")
+  const [countries, setCountries] = useState([])
+
+  useEffect(function(){
+async function fetchCountries(){
+  const res = await fetch(` https://restcountries.com/v3.1/region/${region}`);
+const data = await res.json();
+setCountries(data);
+
+}
+fetchCountries()
+  },[region])
 return (
   <div className="container">
 
   <Header/>
   <Navigation setRegion={setRegion}/>
+  <CountriesList countries ={countries}/>
   </div>
 )
 }
 
+function CountriesList({countries}){
+return (
+  <ul className="country">
+{countries.map((country)=>{
+  <Country  country ={country}/>
+})}
+  </ul>
+)
+}
+
+function Country({country}){
+  return (
+    <li>
+      <img src = {country.flags.png} alt ={`Falg of`}/>
+    </li>
+  )
+}
 function Navigation({region, setRegion}){
   return(
     <div>
