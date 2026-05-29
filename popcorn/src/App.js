b192c441724e6d14bfce213f03f01185227e57fe
Prajjwal Budha <prajjwalbudha@gmail.com>
2026-05-29 18:59:28 +0545

    made custom hook working

--- popcorn/src/App.js
+++ popcorn/src/App.js
@@ -28,48 +28,11 @@ export default function App() {
     localStorage.setItem("watched", JSON.stringify([...watched, movie]));
   }
 
+
   function handleDeleteWatched(id) {
     setWatched((watched) => watched.filter((movie) => movie.imdbID !== id));
   }
-  useMovie()
 
-
-  // useEffect(
-  //   function () {
-  //     const controller = new AbortController();
-  //     async function fetchMovies() {
-  //       try {
-  //         setIsLoading(true);
-  //         setError("");
-  //         const res = await fetch(
-  //           `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
-  //           { signal: controller.signal },
-  //         );
-  //         if (!res.ok)
-  //           throw new Error("Something went wrong with movie fetching");
-  //         const data = await res.json();
-  //         if (data.Response === "False") throw new Error("Movie Not Found");
-  //         setMovies(data.Search);
-  //         setError("");
-  //       } catch (err) {
-  //         if (err.name !== "AbortError") setError(err.message);
-  //       } finally {
-  //         setIsLoading(false);
-  //       }
-  //     }
-  //     if (query.length < 3) {
-  //       setMovies([]);
-  //       setError("");
-  //       return;
-  //     }
-  //     fetchMovies();
-
-  //     return function () {
-  //       controller.abort();
-  //     };
-  //   },
-  //   [query],
-  // );
 
   return (
     <>
