1b87b7ec8299a2225521b34ea9e103e1b575e4fc
Prajjwal Budha <prajjwalbudha@gmail.com>
2026-05-31 22:23:08 +0545

    added Header component

--- reactquiz/src/App.js
+++ reactquiz/src/App.js
@@ -1,9 +1,11 @@
-import DateCounter from "./DateCounter";
+import Header from "./Header";
+
 
 function App() {
   return (
-    <div>
-      <DateCounter/>
+    <div className="App">
+      <Header/>
+      
     </div>
   );
 }
