d3a4dfa83ad1fe630e7b2ed3044c99410acb73cf
Prajjwal Budha <prajjwalbudha@gmail.com>
2026-05-31 20:50:05 +0545

    added reducer

--- reactquiz/src/DateCounter.js
+++ reactquiz/src/DateCounter.js
@@ -1,25 +1,30 @@
-import { useState } from "react";
+import { useReducer, useState } from "react";
+
+function reducer(state, action){
+console.log(state, action)
+}
 
 function DateCounter() {
-  const [count, setCount] = useState(0);
   const [step, setStep] = useState(1);
+  // const [count, setCount] = useState(0);
 
+  const [count, dispatch]= useReducer(reducer, 0);
   // This mutates the date object.
   const date = new Date("june 21 2027");
   date.setDate(date.getDate() + count);
 
   const dec = function () {
     // setCount((count) => count - 1);
-    setCount((count) => count - step);
+    // setCount((count) => count - step);
   };
 
   const inc = function () {
     // setCount((count) => count + 1);
-    setCount((count) => count + step);
+    // setCount((count) => count + step);
   };
 
   const defineCount = function (e) {
-    setCount(Number(e.target.value));
+    // setCount(Number(e.target.value));
   };
 
   const defineStep = function (e) {
@@ -27,7 +32,7 @@ function DateCounter() {
   };
 
   const reset = function () {
-    setCount(0);
+    // setCount(0);
     setStep(1);
   };
 
