8deb3ff8034b98afddb0914de26a6bd763f688c7
Prajjwal Budha <prajjwalbudha@gmail.com>
2026-05-31 21:17:44 +0545

    rewrite the logic of if into switch

--- reactquiz/src/DateCounter.js
+++ reactquiz/src/DateCounter.js
@@ -5,12 +5,24 @@ const initialState = {
   step: 1,
 };
 function reducer(state, action) {
-  console.log(state, action);
+  // console.log(state, action);
   // return state + action;
-  if (action.type === "inc") return state + action.payload;
-  if (action.type === "dec") return state - action.payload;
-  if (action.type === "setCount") return action.payload;
-  return state;
+  // if (action.type === "inc") return state + action.payload;
+  // if (action.type === "dec") return state - action.payload;
+  // if (action.type === "setCount") return action.payload;
+  // return state;
+  switch(action.type){
+    case 'inc':
+      return{...state, count: state.count + action.payload }
+      case 'dec':
+        return{...state, count: state.count - action.payload}
+        case 'setCount': return{
+          ...state, count:action.payload
+        }
+      default:
+        throw new Error('Unknown action')
+  }
+
 }
 
 function DateCounter() {
