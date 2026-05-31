3f19ba76ec10b6be22b0eff11d6fc43a4cf2f8b4
Prajjwal Budha <prajjwalbudha@gmail.com>
2026-05-31 22:37:22 +0545

    fetched data

--- reactquiz/src/App.js
+++ reactquiz/src/App.js
@@ -1,8 +1,15 @@
+import { useEffect } from "react";
 import Header from "./Header";
 import Main from "./Main";
 
 
 function App() {
+  useEffect(function(){
+    fetch("http://localhost:3001/questions")
+    .then((res) => res.json())
+    .then((data) => console.log (data))
+    .then((err) => console.error(err))
+  })
   return (
     <div className="app">
       <Header/>
