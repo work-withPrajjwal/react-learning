d6291ffb2ea0bd46aba00dd5369c82830e659860
Prajjwal Budha <prajjwalbudha@gmail.com>
2026-06-01 17:34:08 +0545

    added error while the status is error

--- reactquiz/src/App.js
+++ reactquiz/src/App.js
@@ -2,6 +2,7 @@ import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
 import Loader from "./Loader"
+import Error from "./Error"
 
 const initialState = {
   questions: [],
@@ -34,6 +35,7 @@ function App() {
       <Header />
       <Main>
        {status === 'loading' && <Loader />}
+       {status === "error" && <Error/>}
       </Main>
     </div>
   );
