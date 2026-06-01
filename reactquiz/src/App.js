e31fa478124f345abe70913f7d5e0810340fb9a8
Prajjwal Budha <prajjwalbudha@gmail.com>
2026-06-01 17:26:45 +0545

    destructured status, questions

--- reactquiz/src/App.js
+++ reactquiz/src/App.js
@@ -2,39 +2,37 @@ import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
 
-
 const initialState = {
   questions: [],
   // loading, error, ready, active, finish
-  status: 'loading'
+  status: "loading",
 };
-function reducer(state, action){
-switch(action.type){
-  case "dataReciebed":
-    return{...state, questions: action.payload, status: 'ready'};
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataRecieved":
+      return { ...state, questions: action.payload, status: "ready" };
     case "dataFailed":
-      return{...state, status:'error'}
-default:
-  throw new Error("Action unknown")
+      return { ...state, status: "error" };
+    default:
+      throw new Error("Action unknown");
   }
 }
 
 function App() {
+  const [{status, questions}, dispatch] = useReducer(reducer, initialState);
 
 
-  const[state, dispatch] = useReducer(reducer, initialState)
-  useEffect(function(){
+  useEffect(function () {
     fetch("http://localhost:3001/questions")
-    .then((res) => res.json())
-    .then((data) => dispatch({type:'datarecieved', payload:data}))
-    .then((err) => dispatch({type:'dataFailed'}))
-  },[])
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataRecieved", payload: data }))
+      .then((err) => dispatch({ type: "dataFailed" }));
+  }, []);
   return (
     <div className="app">
-      <Header/>
+      <Header />
       <Main>
-        <p>1/15</p>
-        <p>questions</p>
+       {state.status === 'loading'}
       </Main>
     </div>
   );
