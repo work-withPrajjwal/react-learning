3f209b861a142f7870e650a73f4c7685bfdcc346
Prajjwal Budha <prajjwalbudha@gmail.com>
2026-06-01 18:28:06 +0545

    added conditional class in button

--- reactquiz/src/Option.js
+++ reactquiz/src/Option.js
@@ -2,7 +2,7 @@ export default function Option({ question, answer, dispatch }) {
   return (
     <div className="options">
       {question.options.map((option, index) => (
-        <button className="btn btn-option" key={option} onClick= {()=>dispatch({type:"newAnswer", payload:index})}>
+        <button className={`btn btn-option ${index===answer ? "answer":""}`} key={option} onClick= {()=>dispatch({type:"newAnswer", payload:index})}>
           {option} 
         </button>
       ))}
