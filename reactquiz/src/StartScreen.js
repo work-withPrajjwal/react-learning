250e7f35bbb71184dd64399b97bbac9d8ffb5423
Prajjwal Budha <prajjwalbudha@gmail.com>
2026-06-01 17:49:30 +0545

    added classname in startScreen

--- reactquiz/src/StartScreen.js
+++ reactquiz/src/StartScreen.js
@@ -1,5 +1,9 @@
 export default function StartScreen() {
   return (
-    <div>StartScreen</div>
+    <div className="start">
+        <h2>Welcome to The React Quiz!</h2>
+        <h3>X questions to test your React Mastery</h3>
+        <button  className="btn btn-ui">Let's Start</button>
+    </div>
   )
 }
