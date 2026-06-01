9102b2998c04b42cabdbb14806dcd79d5e0ca905
Prajjwal Budha <prajjwalbudha@gmail.com>
2026-06-01 18:11:08 +0545

    added key on button and created new componet called Option in Option.js

--- reactquiz/src/Question.js
+++ reactquiz/src/Question.js
@@ -1,11 +1,9 @@
+import Option from "./Option"
 export default function Question({question}) {
   return (
     <div>
         <h4>{question.question}</h4>
-        <div className="options">
-            {question.options.map((option=> <button className="btn btn-option">{option}</button>
-            ))}
-        </div>
+      <Option  question={question}/>
     </div>
   )
 }
