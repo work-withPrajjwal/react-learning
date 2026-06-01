39b7db390e538a13e07497473ae035cbe123ab8d
Prajjwal Budha <prajjwalbudha@gmail.com>
2026-06-01 18:33:16 +0545

    added style according to questions if correct blue and wrong ones with orange

--- reactquiz/src/Option.js
+++ reactquiz/src/Option.js
@@ -1,9 +1,15 @@
 export default function Option({ question, answer, dispatch }) {
+  const hasAnswered = answer !== null;
   return (
     <div className="options">
       {question.options.map((option, index) => (
-        <button className={`btn btn-option ${index===answer ? "answer":""}`} key={option} onClick= {()=>dispatch({type:"newAnswer", payload:index})}>
-          {option} 
+        <button
+          className={`btn btn-option ${index === answer ? "answer" : ""} ${hasAnswered ? (index === question.correctOption ? "correct" : "wrong") : ""}`}
+          key={option}
+          onClick={() => dispatch({ type: "newAnswer", payload: index })}
+          disabled={hasAnswered}
+        >
+          {option}
         </button>
       ))}
     </div>
