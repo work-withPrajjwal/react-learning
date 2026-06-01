ed16a319329828e35cfd3a183f1419191a9eab4b
Prajjwal Budha <prajjwalbudha@gmail.com>
2026-06-01 18:24:18 +0545

    passed index in payload in answer

--- reactquiz/src/Option.js
+++ reactquiz/src/Option.js
@@ -1,9 +1,9 @@
 export default function Option({ question, answer, dispatch }) {
   return (
     <div className="options">
-      {question.options.map((option) => (
-        <button className="btn btn-option" key={option}>
-          {option}
+      {question.options.map((option, index) => (
+        <button className="btn btn-option" key={option} onClick= {()=>dispatch({type:"newAnswer", payload:index})}>
+          {option} 
         </button>
       ))}
     </div>
