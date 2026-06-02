e856922cb9b55597a3def2adf1a17623698e0ccf
Prajjwal Budha <prajjwalbudha@gmail.com>
2026-06-02 22:32:38 +0545

    handled not mentioned link

--- world-wise/src/App.jsx
+++ world-wise/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
+import PageNotFound from "./pages/PageNotFound";
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
  <Route path="/" element={<HomePage/>}/>
  <Route path="/product" element={<Product/>}/>
  <Route path="/pricing" element={<Pricing/>}/>
+ <Route path ="*" element={<PageNotFound/>}/>
     </Routes>
 
     
