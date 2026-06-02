d92c0ae7c51623823fc92e5aed680e5ab54fe514
Prajjwal Budha <prajjwalbudha@gmail.com>
2026-06-02 22:16:26 +0545

    react router

--- world-wise/src/App.jsx
+++ world-wise/src/App.jsx
@@ -1,5 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+
 export default function App() {
   return (
-    <div>Worldwise</div>
+    <BrowserRouter>
+    <Routes>
+
+ <Route path="/" element={<HomePage/>}/>
+ <Route path="/product" element={<Product/>}/>
+ <Route path="/pricing" element={<Pricing/>}/>
+    </Routes>
+
+    
+    </BrowserRouter>
   )
 }
