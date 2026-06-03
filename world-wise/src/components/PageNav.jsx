dfb0e31a8b8887edebdd28e12387d9a4a62bc109
Prajjwal Budha <prajjwalbudha@gmail.com>
2026-06-03 19:43:59 +0545

    added class to ul

--- world-wise/src/components/PageNav.jsx
+++ world-wise/src/components/PageNav.jsx
@@ -4,7 +4,7 @@ import styles from "./PageNav.module.css";
 export default function PageNav() {
   return (
  <nav className={styles.nav}>
-  <ul>
+  <ul className={styles.list}>
     <li><Link to ="/">Home</Link></li>
     <li><Link to ="/product">Product</Link></li>
     <li><Link to ="/pricing">Pricing</Link></li>
