679ccad989c22e5c3dd778516a387e52c167077e
Prajjwal Budha <prajjwalbudha@gmail.com>
2026-06-04 20:44:02 +0545

    fetched data using useEffect

--- world-wise/src/App.jsx
+++ world-wise/src/App.jsx
@@ -7,6 +7,32 @@ import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login"
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
+import { useEffect, useState } from "react";
+
+
+
+const BASE_URL = "http://localhost:8000";
+
+const [cities, setCities] = useState([]);
+const[isLoading, setISLoading]= useState(false);
+
+useEffect(function(){
+  async function fetchCities(){
+   try{ 
+    setISLoading(true);
+    const res = await fetch(`${BASE_URL}/cities`);
+    const data = await res.json();
+    setCities(data);
+  }
+
+  catch(err){
+    console.log('Eror fetching cities', err)
+  }finally{
+    setISLoading(false);
+  }
+}
+fetchCities()
+},[]);
 
 export default function App() {
   return (
