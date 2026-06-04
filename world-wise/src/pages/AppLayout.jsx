ac84152a72d28540d86cb1739e6bcf646a9561eb
Prajjwal Budha <prajjwalbudha@gmail.com>
2026-06-04 19:19:29 +0545

    Sidebar added in AppLayout

--- world-wise/src/pages/AppLayout.jsx
+++ world-wise/src/pages/AppLayout.jsx
@@ -1,9 +1,10 @@
+import Sidebar from '../components/Sidebar'
 import styles from './AppLayout.module.css'
 
 export default function AppLayout() {
   return (
     <div className={styles.app}>
-    
+    <Sidebar/>
     </div>
   )
 }
