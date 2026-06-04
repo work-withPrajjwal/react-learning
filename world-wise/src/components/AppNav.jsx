f650f071cfaffd644c3727db959229fdbf663919
Prajjwal Budha <prajjwalbudha@gmail.com>
2026-06-04 12:39:18 +0545

    AppNav linked

--- world-wise/src/components/AppNav.jsx
+++ world-wise/src/components/AppNav.jsx
@@ -1,13 +1,12 @@
-
-import { Link } from "react-router-dom"
-import styles from "./AppNav.module.css"
+import { Link } from "react-router-dom";
+import styles from "./AppNav.module.css";
 
 export default function AppNav() {
   return (
-   <ul className={styles.nav}>
-    <li className={styles.list}>
-        <link to ="a/app">App</link>
-    </li>
-   </ul>
-  )
+    <ul className={styles.nav}>
+      <li className={styles.list}>
+        <Link to="/app">App</Link>
+      </li>
+    </ul>
+  );
 }
--- world-wise/src/pages/HomePage.jsx
+++ world-wise/src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
+import AppNav from "../components/AppNav";
 import PageNav from "../components/PageNav";
 
 
 export default function HomePage() {
   return (
     <div>
+      <AppNav/>
 
         <PageNav/>
      
