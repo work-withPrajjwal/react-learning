25987b5259f710b1a7f24d8dc1e326c1b621e10d
Prajjwal Budha <prajjwalbudha@gmail.com>
2026-06-05 08:10:39 +0545

    added formatDate in cityItem

--- world-wise/src/components/CItyItem.jsx
+++ world-wise/src/components/CItyItem.jsx
@@ -1,6 +1,15 @@
 /* eslint-disable react/prop-types */
 import { Link } from "react-router-dom";
 import styles from "./CityItem.module.css";
+
+const formatDate = (date) =>
+  new Intl.DateTimeFormat("en", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+    weekday: "long",
+  }).format(new Date(date));
+
 export default function CItyItem({city}) {
 const {cityName, emoji, date}= city;
   return (
@@ -8,7 +17,7 @@ const {cityName, emoji, date}= city;
       <Link className={styles.cityItem}>
       <span className={styles.emoji}>{emoji}</span>
       <h3 className={styles.name}>{cityName}</h3>
-      <time className={styles.date}>{date}</time>
+      <time className={styles.date}>({formatDate(date)})</time>
       </Link>
       </li>
   )
