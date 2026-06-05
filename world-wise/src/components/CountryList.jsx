99d0a4b7f697901ffa743182fa3c0b7553067339
Prajjwal Budha <prajjwalbudha@gmail.com>
2026-06-05 08:16:53 +0545

    Country Item Added

--- world-wise/src/components/CountryList.jsx
+++ world-wise/src/components/CountryList.jsx
@@ -1,7 +1,15 @@
 import styles from "./CountryList.module.css"
+import Spinner from "./Spinner";
+import CountryItem from "./CountryItem"
 
-export default function CountryList() {
+export default function CountryList({cities, isLoading}) {
+  if (isLoading) return <Spinner />;
+
   return (
-    <div className={styles.countrylist}>C</div>
-  )
+    <ul className={styles.cityList}>
+      {cities.map((city) => (
+        <CountryItem city={city} key={cities.id} />
+      ))}
+    </ul>
+  );
 }
