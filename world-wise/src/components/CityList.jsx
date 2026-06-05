9a2d27ae51dd111e3f4dfd1b4fce59ef33966e5d
Prajjwal Budha <prajjwalbudha@gmail.com>
2026-06-05 08:46:02 +0545

    added Message component if data is empty

--- world-wise/src/components/CityList.jsx
+++ world-wise/src/components/CityList.jsx
@@ -2,9 +2,11 @@
 import CityItem from "./CityItem";
 import Spinner from "./Spinner";
 import styles from "./CityList.module.css";
+import Message from "./Message"
 
 export default function CityList({ cities, isLoading }) {
   if (isLoading) return <Spinner />;
+  if(!cities.length) return <Message message="add your first city by clicking on the map" />;
 
   return (
     <ul className={styles.cityList}>
--- world-wise/src/components/CountryList.jsx
+++ world-wise/src/components/CountryList.jsx
@@ -1,15 +1,17 @@
 import styles from "./CountryList.module.css"
 import Spinner from "./Spinner";
 import CountryItem from "./CountryItem"
+import Message from "./Message";
 
 export default function CountryList({cities, isLoading}) {
-  if (isLoading) return <Spinner />;
-
   const countries = cities.reduce((arr, city) => {
     if (!arr.map((el) => el.country).includes(city.country))
       return [...arr, { country: city.country, emoji: city.emoji }];
     else return arr;
   }, []);
+  if (isLoading) return <Spinner />;
+  if(!countries.length) return <Message message="add your first city by clicking on the map"/>
+
   return (
     <ul className={styles.countryList}>
       {countries.map((country) => (
