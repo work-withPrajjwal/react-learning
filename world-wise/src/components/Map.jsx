6bd20e224a71e07c66dd9ac92deb63ba6a72a651
Prajjwal Budha <prajjwalbudha@gmail.com>
2026-06-07 21:29:49 +0545

    used searchParam hooks in Map component

--- world-wise/src/components/Map.jsx
+++ world-wise/src/components/Map.jsx
@@ -1,6 +1,9 @@
+import { useSearchParams } from "react-router-dom"
 import styles from "./Map.module.css"
 
 export default function Map() {
+  const[searchParams, setSearchParams]=useSearchParams();
+  
   return (
     <div className={styles.mapContainer}>Map</div>
   )
