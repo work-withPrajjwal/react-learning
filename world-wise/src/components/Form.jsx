b3579e566530c5264e7c8523d11c36255e475e1c
Prajjwal Budha <prajjwalbudha@gmail.com>
2026-06-07 22:09:55 +0545

    back button functionality used

--- world-wise/src/components/Form.jsx
+++ world-wise/src/components/Form.jsx
@@ -1,6 +1,7 @@
 // "https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=0&longitude=0"
 
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 import styles from "./Form.module.css";
 import Button from "./Button";
@@ -14,6 +15,7 @@ export function convertToEmoji(countryCode) {
 }
 
 function Form() {
+  const navigate = useNavigate()
   const [cityName, setCityName] = useState("");
   const [country, setCountry] = useState("");
   const [date, setDate] = useState(new Date());
@@ -51,7 +53,9 @@ function Form() {
 
       <div className={styles.buttons}>
         <Button type='primary'>Add</Button>
-        <Button type='back'> &larr; Back</Button>
+        <Button type='back' onClick={(e)=> {
+          e.preventDefault();
+          navigate(-1)}}> &larr; Back</Button>
       </div>
     </form>
   );
