9122c8989db0ee9cfa307f8e3aa675e5ef466fb4
Prajjwal Budha <prajjwalbudha@gmail.com>
2026-06-07 21:24:49 +0545

    passed lng in query string

--- world-wise/src/components/CItyItem.jsx
+++ world-wise/src/components/CItyItem.jsx
@@ -14,7 +14,7 @@ export default function CItyItem({city}) {
 const {cityName, emoji, date, id, position}= city;
   return (
     <li>
-      <Link className={styles.cityItem} to={`${id}?lat=${position.lat}`}>
+      <Link className={styles.cityItem} to={`${id}?lat=${position.lat}&lng=${position.lng}}`}>
       <span className={styles.emoji}>{emoji}</span>
       <h3 className={styles.name}>{cityName}</h3>
       <time className={styles.date}>({formatDate(date)})
