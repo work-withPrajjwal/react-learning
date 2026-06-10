c96a77a8d7a783843c4660c2f373410636ba3cba
Prajjwal Budha <prajjwalbudha@gmail.com>
2026-06-10 09:39:51 +0545

    Added conditional className

--- world-wise/src/components/CItyItem.jsx
+++ world-wise/src/components/CItyItem.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 import { Link } from "react-router-dom";
 import styles from "./CityItem.module.css";
+import { useCities } from "../contexts/CitiesContext";
 
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
@@ -12,9 +13,10 @@ const formatDate = (date) =>
 
 export default function CItyItem({city}) {
 const {cityName, emoji, date, id, position}= city;
+const {currentCity}= useCities();
   return (
     <li>
-      <Link className={styles.cityItem} to={`${id}?lat=${position.lat}&lng=${position.lng}}`}>
+      <Link className={`${styles.cityItem} ${id ===currentCity.id ? styles["cityItem--active"]:""}`} to={`${id}?lat=${position.lat}&lng=${position.lng}}`}>
       <span className={styles.emoji}>{emoji}</span>
       <h3 className={styles.name}>{cityName}</h3>
       <time className={styles.date}>({formatDate(date)})
