aef3613414956e4c56113c86f3447f6072e81b58
Prajjwal Budha <prajjwalbudha@gmail.com>
2026-06-10 11:17:52 +0545

    changed content in popup

--- world-wise/src/components/Map.jsx
+++ world-wise/src/components/Map.jsx
@@ -1,11 +1,20 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
-import { MapContainer, Marker, Popup, TileLayer, useMap, useMapEvent, useMapEvents } from "react-leaflet";
+import {
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+  useMap,
+  useMapEvents,
+} from "react-leaflet";
 import { useEffect, useState } from "react";
 import { latLng } from "leaflet";
+import { useCities } from "../contexts/CitiesContext";
 
 export default function Map() {
-  const navigate = useNavigate();
+  const { cities } = useCities();
+
   const [searchParams, setSearchParams] = useSearchParams();
   const [mapPosition, setMapPosition] = useState([27.7172, 85.324]);
 
@@ -26,19 +35,22 @@ export default function Map() {
         center={mapPosition}
         // center={mapPosition}
         zoom={6}
-        scrollWheelZoom={true}        
+        scrollWheelZoom={true}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.fr/hot/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={mapPosition}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        {cities.map((city) => (
+          <Marker position={[city.position.lat, city.position.lng]} key={city.id}>
+            <Popup>
+              <span>{city.emoji}</span>
+              <span>{city.cityName}</span>
+            </Popup>
+          </Marker>
+        ))}
         <ChangeCenter position={mapPosition} />
-        <DetectClick/>
+        <DetectClick />
       </MapContainer>
     </div>
   );
@@ -49,14 +61,13 @@ function ChangeCenter({ position }) {
   map.setView(position);
 
   return null;
-}                 
+}
 
-
-function DetectClick(){
+function DetectClick() {
   const navigate = useNavigate();
   useMapEvents({
-    click: (e)=>{
-      navigate(`form?lat=${e.latLng.lat}&lng=${e.latlng.lng}`);
+    click: (e) => {
+      navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`);
     },
   });
 }
