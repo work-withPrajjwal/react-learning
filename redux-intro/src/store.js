43af33c0f2a8da4952e0df262c9d9f2cf84d84cd
Prajjwal Budha <prajjwalbudha@gmail.com>
2026-06-23 21:02:33 +0545

    added middleware inside composewithdebvtools

--- redux-intro/src/store.js
+++ redux-intro/src/store.js
@@ -3,6 +3,7 @@ import {applyMiddleware, combineReducers, createStore} from "redux";
 import {thunk} from "redux-thunk";
 import accountReducer from "./features/account/accountSlice";
 import customerReducer from "./features/customers/customerSlice";
+import { composeWithDevTools } from "@redux-devtools/extension";
 
 
 
@@ -14,6 +15,9 @@ const rootReducer = combineReducers({
 
 
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk)),
+);
 
 export default store;
