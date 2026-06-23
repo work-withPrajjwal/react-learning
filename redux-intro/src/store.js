d58d5a0f1e0b9df72bba3408d333a256cd1067d5
Prajjwal Budha <prajjwalbudha@gmail.com>
2026-06-23 21:44:36 +0545

    added features of reduxtoolkit

--- redux-intro/src/store.js
+++ redux-intro/src/store.js
@@ -1,22 +1,12 @@
-import {applyMiddleware, combineReducers, createStore} from "redux";
-import {thunk} from "redux-thunk";
+import { configureStore } from "@reduxjs/toolkit";
 import accountReducer from "./features/account/accountSlice";
 import customerReducer from "./features/customers/customerSlice";
-import { composeWithDevTools } from "@redux-devtools/extension";
 
 
-
-
-const rootReducer = combineReducers({
+configureStore({reducer:{
   account: accountReducer,
-  customer: customerReducer,
-});
+  customerReducer,  
+}})
 
-
-
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(thunk)),
-);
 
 export default store;
